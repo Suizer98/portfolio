837eeb134adc3aefdd8d70c47242926272577acc
Suizer98 <93068036+Suizer98@users.noreply.github.com>
2025-02-07 20:32:22 +0800

    Edited about me

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,8 +8,9 @@ function About() {
     <section className={classes.about}>
       <h2>About me</h2>
       <p className={classes.text}>
-        Not just a typical software engineer proficient in the latest stacks; but also possess
-        expertise in GIS, marine sciences, and related scientific domains.
+        Not just a typical software engineer proficients in the latest stacks; but also possess expertise in GIS, marine sciences, 
+        and related scientific domains. Having experiences collaborating with scientists and engineers worldwide, 
+        I excel in managing GIS webpages and conducting researches under NUS coverages.
       </p>
 
       {ExperiencesData.map((el) => (
