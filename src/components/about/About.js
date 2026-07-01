3cafbe4d0621ee72549fb5c3323746b5e0ddd5da
Suizer <93068036+Suizer98@users.noreply.github.com>
2026-07-01 19:33:19 +0800

    Update about section with improved description
    Revised the about section to better highlight cross-disciplinary expertise in GIS and software development.

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,10 +8,10 @@ function About() {
     <section className={classes.about}>
       <h2>About me</h2>
       <p className={classes.text}>
-        Not just a typical software engineer proficients in the latest stacks; but also possess
-        knowledges in GIS, marine sciences, and related scientific domains. Having experiences
-        collaborating with scientists and engineers worldwide, I excel in developing GIS web
-        services and solving challenges.
+        My cross-disciplinary expertises enables me to excel in both domains, 
+        from collecting real world GIS data, processing them to developing innovative software 
+        solutions tailored to meet spatial data needs. My knowledges and skills allow me to bridge 
+        the gap between geomatics engineering and gis software development.
       </p>
 
       {ExperiencesData.map((el, index) => (
